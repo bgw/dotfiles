d646396fafefcf1ae4be6a8b9723c104732bfb27
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2012-12-19 23:32:11 -0500

    Adds gitignore file
    The gitignore will ignore vim swap files.

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ SIMPLE_FILES[0]="tmux.conf"
 SIMPLE_FILES[1]="vimrc"
 SIMPLE_FILES[2]="zshrc"
 SIMPLE_FILES[3]="gitconfig"
+SIMPLE_FILES[4]="gitignore"
 
 if [ $# -eq 1 ] && ( [ $1 = "-d" ] || [ $1 = "--delete" ] ); then
     echo "Warning: Removing old install"
