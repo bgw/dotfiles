0433e9828ba3ef487f3ea15d612499d7a10c8e8e
Benjamin Woodruff <github@benjam.info>
2016-01-19 00:04:40 -0500

    [zsh] Shut up if keychain binary isn't in PATH

--- zsh/commands/ssh.zsh
+++ zsh/commands/ssh.zsh
@@ -1,13 +1,13 @@
 # We use xterm-256color-italic, which won't be supported on other hosts
-alias ssh="hash keychain && start-keychain -q; TERM=xterm-256color ssh"
-alias mosh="hash keychain && start-keychain -q; TERM=xterm-256color mosh"
+alias ssh="hash keychain 2>/dev/null && start-keychain -q; TERM=xterm-256color ssh"
+alias mosh="hash keychain 2>/dev/null && start-keychain -q; TERM=xterm-256color mosh"
 
 KEYCHAIN_SSH_KEYS="id_rsa"
 KEYCHAIN_GPG_KEYS="4E2569F1"
 
 start-keychain() {
     # http://www.funtoo.org/Keychain#Linux
-    if hash gpg-agent; then
+    if hash gpg-agent 2> /dev/null; then
         eval $(keychain --eval --agents gpg,ssh "$@" \
                $KEYCHAIN_SSH_KEYS)
     else
