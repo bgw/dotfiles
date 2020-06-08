f78fda52bccd9a3cfa9013c09ba1a1165671d259
Benjamin Woodruff <github@benjam.info>
2020-06-07 22:21:48 -0700

    [zsh] Remove unused stuff from zsh/commands
    - I don't use virtualbox to manage Debian VMs anymore. When I need to do
      this, I use lxd and libvirt/kvm instead.
    - I don't use proot anymore. I use lxd instead.
    - I don't use the UF CISE shell servers anymore.
    - Remove sagemath stuff.
    - Remove evince wrapper for tmux.

--- zsh/commands/sage.zsh
+++
@@ -1,8 +0,0 @@
-# Alias Sage and setup SageTeX
-export SAGE_ROOT="$HOME/applications/$( \
-    for alternative in "$(ls $HOME/applications/ 2> /dev/null | grep ^sage)"; \
-        do echo "$alternative"; done \
-    | sort | tail -n 1 | tr -d "\n" \
-)" # finds the latest version of sage installed
-alias sage="$SAGE_ROOT/sage"
-export TEXINPUTS="$SAGE_ROOT/local/share/texmf//:"
