b1ad97ab48980ce162ac8d6ff4c1e5ca73e52457
Benjamin Woodruff <github@benjam.info>
2024-04-21 20:14:17 -0700

    [vim] Disable some annoying lazyvim options
    - Disable `<A-j>`/`<A-k>` keybinds due to false-triggers with ESC.
    - Disable relativenumber, the additional redraw isn't worth it.
    - Disable `conceallevel` by default, it's nice for reading markdown, but
      not for trying to write it. `<leader>uc` can toggle it.
    - Disable `mini.pairs`. It gets in the way far more often than it helps.

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- in theory, these are useful, but nvim/terminals often interpret ESC followed
+-- by a quick movement (j/k) as holding Alt and pressing j/k, so it causes too
+-- many false triggers, see `:help map-alt-keys`
+vim.keymap.del({"n", "i", "v"}, "<A-j>")
+vim.keymap.del({"n", "i", "v"}, "<A-k>")
--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,5 +1,10 @@
-vim.lsp.inlay_hint = true
-vim.opt.scrolloff = 2
-vim.opt.colorcolumn = "+1"
 vim.opt.textwidth = 80
+vim.opt.colorcolumn = "+1"
+
+vim.opt.scrolloff = 2
+vim.opt.relativenumber = false
+vim.opt.conceallevel = 0
+
+vim.lsp.inlay_hint = true
+
 vim.api.nvim_create_user_command("Strp", [[%s/\s\+\n/\r/g]], {})
