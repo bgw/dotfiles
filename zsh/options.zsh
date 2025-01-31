4981f28494f2f7c75b54f1d61e6b67690e024622
Benjamin Woodruff <github@benjam.info>
2025-01-30 17:52:37 -0800

    [zsh] Remove TERM=xterm-256color hack
    This subtly breaks ghostty.

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -18,14 +18,6 @@ setopt hist_ignore_all_dups
 setopt hist_ignore_space
 unsetopt share_history # oh-my-zsh sets this, I don't want it.
 
-if [[ $TERM = xterm* ]]; then
-    # Use 256 Colors! (in xterm and things that report themselves as xterm, like
-    # gnome-terminal)
-    export TERM=xterm-256color
-elif [[ $TERM = screen* ]]; then
-    export TERM=screen-256color
-fi
-
 # Make cd act like pushd instead, letting us use popd to go back
 setopt autopushd
 
